cf90219ff9da955634335b615785e554093179cf
Feras Deiratany <deiratany.1@osu.edu>
2019-05-08 13:17:28 -0400

    added image

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          {'Pickle Sundae'}
         </Link>
       </h1>
     </div>
